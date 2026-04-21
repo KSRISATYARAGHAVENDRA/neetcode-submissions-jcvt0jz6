e796808d124f5b8a6e674cb71e0091245202b7d5
KSRISATYARAGHAVENDRA <raghukodavati2@gmail.com>
2026-04-21 23:01:45 +0530

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.java
@@ -0,0 +1,44 @@
+// sliding window with hash map
+class Solution {
+    public String minWindow(String s, String t) {
+        if(t.isEmpty() || t.length() > s.length()) return "";
+        
+        int[] res = {-1, -1};
+        int reslen = Integer.MAX_VALUE;
+
+        Map<Character ,Integer> tcount = new HashMap<>();
+        Map<Character ,Integer> window = new HashMap<>();
+
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            tcount.put(c, tcount.getOrDefault(c, 0) + 1);
+        }
+        
+        int need = tcount.size();
+        int have = 0;
+        int l = 0;
+        for(int r = 0; r < s.length(); r++){
+            char c = s.charAt(r);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if(tcount.containsKey(c) && tcount.get(c).equals(window.get(c))){
+                have++;
+            }
+
+            while(have == need){
+                if(r - l + 1 < reslen){
+                    reslen = r - l + 1;
+                    res[0] = l;
+                    res[1] = r;
+                }
+                char left = s.charAt(l);
+                window.put(left, window.get(left) - 1);
+                if(tcount.containsKey(left) && tcount.get(left) > window.get(left)){
+                    have--;
+                }
+                l++;
+            }
+        }
+        return reslen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+    }
+}
